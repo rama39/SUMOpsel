e2fba866271aef9835551a108dc8718e6a16ce0c
rama39 <rama@cin.ufpe.br>
2025-04-24 22:56:33 +0000

    V1: (refactor:lib) consertar

--- Software/test/CONTROLE/CONTROLE.ino
+++ Software/test/CONTROLE/CONTROLE.ino
@@ -1,6 +1,60 @@
 
+//================================================================================================
+//================================================================================================
+
 // Biblio do controle dos motores
-#include "../../lib/controle/cont.h"
+/*
+    Biblioteca para controle dos motores do robô com ponte H l298n mini
+*/
+
+#define A1 25
+#define A2 26
+#define B1 27
+#define B2 14
+
+void setupContr();
+
+void controle(int,int,int,int);
+void contrPara();
+void contrFren();
+void contrDire();
+void contrEsqu();
+
+//------------------------------------------------------------------------------------------------
+
+void setupContr() {
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(B1, OUTPUT);
+  pinMode(B2, OUTPUT);
+}
+
+void controle(int IN1, int IN2, int IN3, int IN4) {
+  digitalWrite(A1, IN1);
+  digitalWrite(A2, IN2);
+  digitalWrite(B1, IN3);
+  digitalWrite(B2, IN4);
+}
+
+// TODO: achar valores que façam sentido pra isso (testando)
+void contrPara() {
+  controle(LOW, LOW, LOW, LOW);
+}
+void contrFren() {
+  controle(HIGH, LOW, HIGH, LOW);
+}
+void contrTras() {
+  controle(LOW, HIGH, LOW, HIGH);
+}
+void contrDire() {
+  controle(HIGH, LOW, LOW, HIGH);
+}
+void contrEsqu() {
+  controle(LOW, HIGH, HIGH, LOW);
+}
+
+//================================================================================================
+//================================================================================================
 
 void setup() {
   
